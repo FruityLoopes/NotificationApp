da0b456912219425d42a2d76f099b3ded62d9ca6
FruityLoopes <chadandrews07@gmail.com>
2023-09-18 10:59:13 +0200

    Added Notification Code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:core_version")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
